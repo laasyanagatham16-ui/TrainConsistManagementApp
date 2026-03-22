ca1b5c7040764d07604918ffdb28e16ef36d9baa
LAASYA REDDY N <laasyanagatham16@gmail.com>
2026-03-22 23:16:56 +0530

    UC1 - Basic Train Consist Setup

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,49 @@
 /*
+ * Train Consist Management App
+ *
  * UC1: Basic Train Consist Setup
- * Demonstrates creation of train coaches
+ * This use case demonstrates the creation of train coaches
+ * and displaying their details.
  */
 
 class Coach {
-    String coachId;
-    String type;
+    private String coachId;
+    private String coachType;
 
-    Coach(String coachId, String type) {
+    // Constructor
+    public Coach(String coachId, String coachType) {
         this.coachId = coachId;
-        this.type = type;
+        this.coachType = coachType;
     }
 
-    void display() {
-        System.out.println(coachId + " - " + type);
+    // Getter methods
+    public String getCoachId() {
+        return coachId;
+    }
+
+    public String getCoachType() {
+        return coachType;
+    }
+
+    // Display method
+    public void displayCoach() {
+        System.out.println("Coach ID: " + coachId + ", Type: " + coachType);
     }
 }
 
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("=== Train Consist Management System ===");
+
+        // Creating coaches
         Coach c1 = new Coach("C1", "Sleeper");
         Coach c2 = new Coach("C2", "AC");
+        Coach c3 = new Coach("C3", "General");
 
-        c1.display();
-        c2.display();
+        // Displaying coach details
+        c1.displayCoach();
+        c2.displayCoach();
+        c3.displayCoach();
     }
 }
