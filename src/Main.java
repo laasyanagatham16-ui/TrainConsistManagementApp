6ce1e0602bf5ddbe449d4231f66ad46a7562a665
LAASYA REDDY N <laasyanagatham16@gmail.com>
2026-03-22 23:13:25 +0530

    UC1 - Basic Train Setup

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,29 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+/*
+ * UC1: Basic Train Consist Setup
+ * Demonstrates creation of train coaches
+ */
+
+class Coach {
+    String coachId;
+    String type;
+
+    Coach(String coachId, String type) {
+        this.coachId = coachId;
+        this.type = type;
+    }
+
+    void display() {
+        System.out.println(coachId + " - " + type);
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Coach c1 = new Coach("C1", "Sleeper");
+        Coach c2 = new Coach("C2", "AC");
+
+        c1.display();
+        c2.display();
     }
 }
